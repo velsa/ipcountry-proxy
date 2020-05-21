6339581b230674c6fb684eb3cd92040b1c01087f
vels <velshome@yahoo.com>
2020-05-21 15:09:14 +0200

    run on my srv

--- src/app.ts
+++ src/app.ts
@@ -184,8 +184,9 @@ app.get('/metrics', async (req, res) => {
 });
 
 // Show quert as JSON
-app.get('/test', async (req, res) => {
-  // console.log('req.query', req.query);
+app.post('/test', async (req, res) => {
+  console.log('req.query', req.query);
+  console.log('req.body', req.body);
   res.header('Content-Type', 'application/json');
   res.status(200).send(req.query);
 });
--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ const DEFAULT_MAX_REQ_PER_HOUR =
   process.env.NODE_ENV === 'test' || process.env.DEBUG ? 3 : 10;
 
 export default {
-  LISTEN_PORT: process.env.LISTEN_PORT || 3000,
+  LISTEN_PORT: process.env.PORT || 3100,
   IS_DEBUG: process.env.DEBUG ? true : false,
   IS_TEST: process.env.NOVE_ENV === 'test' ? true : false,
   maxStatsCache: process.env.MAX_STAT_CACHE || 100, // must be larger than ANY maxRequestsPerHour!
